c0a25ae2b695df0e619fbfa99a3a2e5174b783b0
iconiclearningcentre <iconiclearningcentre.mm@gmail.com>
2026-07-22 15:49:50 +0630

    Update questions.js

--- placement/questions.js
+++ placement/questions.js
@@ -1,1 +1,130 @@
+const questions = [
+  {
+    q: "I ______ bus on Mondays.",
+    options: [
+      "'m going to work with",
+      "'m going to work by",
+      "go to work with",
+      "go to work by"
+    ],
+    correct: 3
+  },
+  {
+    q: "Sorry, but this chair is ______.",
+    options: ["me", "mine", "my", "our"],
+    correct: 1
+  },
+  {
+    q: "A: 'How old ______?' B: 'I ______.'",
+    options: [
+      "are you / am 20 years old.",
+      "have you / have 20 years old",
+      "are you / am 20 years.",
+      "do you have / have 20 years."
+    ],
+    correct: 0
+  },
+  {
+    q: "I ______ to the cinema.",
+    options: [
+      "not usually go",
+      "don't usually go",
+      "don't go usually",
+      "do not go usually"
+    ],
+    correct: 1
+  },
+  {
+    q: "Where ______ ?",
+    options: [
+      "your sister works",
+      "your sister work",
+      "does your sister work",
+      "do your sister work"
+    ],
+    correct: 2
+  },
+  {
+    q: "The test is ______ February.",
+    options: ["in", "at", "on", "over"],
+    correct: 0
+  },
+  {
+    q: "I eat pasta ______ week.",
+    options: ["twice in a", "twice a", "one time a", "once in a"],
+    correct: 1
+  },
+  {
+    q: "I don't have ______ free time.",
+    options: ["many", "any", "a lot", "some"],
+    correct: 1
+  },
+  {
+    q: "A: '_____ to the cinema tomorrow?'",
+    options: ["We will go", "Do we go", "We go", "Shall we go"],
+    correct: 3
+  },
+  {
+    q: "We went to the market ______ some vegetables.",
+    options: ["to buy", "for buy", "for to buy", "for buying"],
+    correct: 0
+  },
+
+  // Page 2
+  {
+    q: "Sorry, but when you called I ______ a shower.",
+    options: ["had", "did have", "was having", "were having"],
+    correct: 2
+  },
+  {
+    q: "______ are very friendly and very intelligent.",
+    options: ["Dolphins", "The dolphins", "A dolphin", "The dolphin"],
+    correct: 0
+  },
+  {
+    q: "Somebody stole ______ yesterday.",
+    options: [
+      "the car of my mother",
+      "my car mother",
+      "my mother's car",
+      "my mother car"
+    ],
+    correct: 2
+  },
+  {
+    q: "______ with me?",
+    options: [
+      "Do you like to dance",
+      "Would you like to dance",
+      "Do you like dance",
+      "Would you like dancing"
+    ],
+    correct: 1
+  },
+  {
+    q: "She is ______ her sister, I think.",
+    options: [
+      "more happier than",
+      "more happy that",
+      "happier that",
+      "happier than"
+    ],
+    correct: 3
+  },
+  {
+    q: "I couldn't eat ______ before the exam.",
+    options: ["nothing", "anything", "everything", "something"],
+    correct: 1
+  },
+  {
+    q: "Please, pass me the remote. ______ TV.",
+    options: [
+      "I'm watching",
+      "I will watch",
+      "I'm going to watch",
+      "I might watch"
+    ],
+    correct: 0
+  }
+];
 
