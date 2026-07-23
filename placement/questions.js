df356a69f80c6b9caa54becdcbad70d1d82e91a4
iconiclearningcentre <iconiclearningcentre.mm@gmail.com>
2026-07-23 16:53:01 +0630

    Update questions.js

--- placement/questions.js
+++ placement/questions.js
@@ -1,21 +1,58 @@
 const questions = [
   // Page 1
-  { q: "I ______ bus on Mondays.", options: [...], correct: 3 },
-  { q: "Sorry, but this chair is ______.", options: [...], correct: 1 },
-  ...
+  {
+    q: "I ______ bus on Mondays.",
+    options: [
+      "'m going to work with",
+      "'m going to work by",
+      "go to work with",
+      "go to work by"
+    ],
+    correct: 3
+  },
+  {
+    q: "Sorry, but this chair is ______.",
+    options: ["me", "mine", "my", "our"],
+    correct: 1
+  },
+  {
+    q: "A: 'How old ______?' B: 'I ______.'",
+    options: [
+      "are you / am 20 years old.",
+      "have you / have 20 years old",
+      "are you / am 20 years.",
+      "do you have / have 20 years."
+    ],
+    correct: 0
+  },
   // Page 2
-  { q: "Sorry, but when you called I ______ a shower.", options: [...], correct: 2 },
-  ...
+  {
+    q: "Sorry, but when you called I ______ a shower.",
+    options: ["had", "did have", "was having", "were having"],
+    correct: 2
+  },
   // Page 3
-  { q: "She ______ to London last week.", options: [...], correct: 2 },
-  ...
+  {
+    q: "She ______ to London last week.",
+    options: ["go", "goes", "went", "gone"],
+    correct: 2
+  },
   // Page 4
-  { q: "If it rains, we ______ stay at home.", options: [...], correct: 0 },
-  ...
+  {
+    q: "If it rains, we ______ stay at home.",
+    options: ["will", "would", "are", "did"],
+    correct: 0
+  },
   // Page 5
-  { q: "They ______ in Yangon since last year.", options: [...], correct: 2 },
-  ...
+  {
+    q: "They ______ in Yangon since last year.",
+    options: ["live", "lived", "have lived", "are living"],
+    correct: 2
+  },
   // Page 6
-  { q: "I ______ coffee every morning.", options: [...], correct: 0 },
-  ...
+  {
+    q: "I ______ coffee every morning.",
+    options: ["drink", "drinks", "drank", "am drinking"],
+    correct: 0
+  }
 ];
